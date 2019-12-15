a20b210bdf7b445a6f94f727b19b21fc025fcdeb
nickpschmidt <nschmidt@bldsllc.com>
2019-12-15 12:43:35 -0500

    Updating disparity testing.

--- data/scripts/disp_tests.py
+++ data/scripts/disp_tests.py
@@ -15,35 +15,46 @@ DISCLAIMER: This notebook is not legal compliance advice.
 import pandas as pd
 import numpy as np
 from data.scripts.di_testing import DisparityTesting
+import os
+import matplotlib.pyplot as plt
 
 pd.set_option('display.max_columns', 30)
 pd.set_option('display.width', 200)
 
-hmda = pd.read_csv('./data/output/test_hmda_with_preds.csv')
+if __name__ == '__main__':
 
-# pg_names = ["black", "amind", "hispanic", "female"]
-# cg_names = ["white", "white", "non_hispanic", "male"]
-# pgcg_names = ["black", "amind", "white", "hispanic", "non_hispanic", "female", "male"]
-pg_names = ["black", "female"]
-cg_names = ["white", "male"]
-pgcg_names = ["black", "white", "female", "male"]
+    pg_names = ["black", "native_american", "asian", "hispanic", "female"]
+    cg_names = ["white", "white", "white", "non_hispanic", "male"]
+    pgcg_names = ["black", "native_american", "asian", "white", "hispanic", "non_hispanic", "female", "male"]
+    # pg_names, cg_names, pgcg_names = ["black", "female"], ["white", "male"], ["black", "white", "female", "male"]
 
-predicted = "high_priced_mgbm_pred"
-label = "high_priced"
-outcome = "decision"
-pred_prob_for_outcome = 0.30
-higher_score_favorable = False
+    file_name = 'test_hmda_with_preds.csv'
+    predicted = "high_priced_mgbm_pred"
+    model_name = "HMDA Monotonic GBM"
+    label = "high_priced"
+    outcome = "decision"
+    pred_prob_for_outcome = 0.30
+    higher_score_favorable = False
 
-hmda[outcome] = np.where(hmda[predicted] >= pred_prob_for_outcome, 1, 0)
-hmda[outcome].value_counts(normalize=True)
+    data = pd.read_csv(os.path.join('./data/output/', file_name))
+    if "native_american" in pg_names:
+        data.rename(inplace=True, columns={'amind': 'native_american'})
+    data[outcome] = np.where(data[predicted] >= pred_prob_for_outcome, 1, 0)
+    data[outcome].value_counts(normalize=True)
 
-data = hmda[["Id", predicted, outcome, label] + pgcg_names]
+    disparity_tests = DisparityTesting(pg_names=pg_names, cg_names=cg_names, pgcg_names=pgcg_names,
+                                       higher_score_favorable=higher_score_favorable)
 
-disp_tests = DisparityTesting(pg_names=pg_names, cg_names=cg_names, pgcg_names=pgcg_names,
-                              higher_score_favorable=higher_score_favorable)
+    cat_outcomes = disparity_tests.categorical_disparity_measures(data=data, label=label, outcome=outcome)
+    cont_outcomes = disparity_tests.continuous_disparity_measures(data=data, predicted=predicted)
+    disparity_measures = DisparityTesting.create_combined_output(cat_outcomes=cat_outcomes, cont_outcomes=cont_outcomes)
 
-cat_outcomes = disp_tests.categorical_disparity_measures(data=data, label=label, outcome=outcome)
-cont_outcomes = disp_tests.continuous_disparity_measures(data=data, predicted=predicted)
-disparity_measures = DisparityTesting.create_combined_output(cat_outcomes=cat_outcomes, cont_outcomes=cont_outcomes)
-
-
+    plt.title("Adverse Impact Ratios for the " + model_name + " Model")
+    plt.xlabel('Protected Classes')
+    plt.legend(loc="upper left")
+    plt.xticks(rotation=-90)
+    plt.bar(x=disparity_measures.loc[disparity_measures["Adverse Impact Ratio"].notna(), "Class"],
+            height=disparity_measures.loc[disparity_measures["Adverse Impact Ratio"].notna(), "Adverse Impact Ratio"],
+            color='blue',
+            )
+    plt.show()
